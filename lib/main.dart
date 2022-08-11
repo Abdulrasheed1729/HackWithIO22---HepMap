b4e7729836c3ef3b57be5974f0f0332405314034
Abdulrasheed Fawole <fawomath@gmail.com>
2022-08-12 00:15:27 +0100

    upgrade dependencies changes from `BlocOverrides.runzone` to `Bloc.observer`

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/view/app.dart';
 import 'bloc_observer.dart';
 
-Future<void> main() {
-  return BlocOverrides.runZoned(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
-      // await Firebase.initializeApp();
-      // final authRepository = AuthRepository();
-      // await authRepository.user.first;
-      runApp(
-        const App(
-            // authRepository: authRepository,
-            ),
-      );
-    },
-    blocObserver: AppBlocObserver(),
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
+  // await Firebase.initializeApp();
+  // final authRepository = AuthRepository();
+  // await authRepository.user.first;
+  runApp(
+    const App(
+        // authRepository: authRepository,
+        ),
   );
 }
