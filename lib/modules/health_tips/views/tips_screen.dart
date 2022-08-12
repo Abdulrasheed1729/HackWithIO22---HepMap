c2a94729cf7bfb3a616043f060359d488319763d
Abdulrasheed Fawole <fawomath@gmail.com>
2022-08-12 11:24:29 +0100

    chore: created health tip screen

--- lib/modules/health_tips/views/tips_screen.dart
+++ lib/modules/health_tips/views/tips_screen.dart
@@ -1,12 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hack_with_io/app/app.dart';
 
 class MyTipsScreen extends StatelessWidget {
   const MyTipsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25.0,
+          vertical: 15.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  padding: const EdgeInsets.all(0.0),
+                  alignment: Alignment.centerLeft,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: SvgPicture.asset(
+                    'assets/icons/Arrow-left.svg',
+                    height: 45,
+                    width: 45,
+                  ),
+                  color: AppColors.kBlackColor,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  'My Tips',
+                  textAlign: TextAlign.center,
+                  style: kHeaderTaglineTextStyle.copyWith(
+                    color: AppColors.kBlackColor,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30.0),
+            Text(
+              'Home-made healthy drinks',
+              textAlign: TextAlign.center,
+              style: kSmallLogoTextStyle.copyWith(
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w600,
+                fontSize: 14.0,
+              ),
+            ),
+            const SizedBox(height: 35.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircleAvatar(
+                  backgroundImage:
+                      AssetImage('assets/images/Healthy-drink.jpg'),
+                  radius: 45.0,
+                ),
+              ],
+            ),
+            const SizedBox(height: 35.0),
+            DecoratedBox(
+              decoration: const BoxDecoration(),
+              child: Text(
+                'Imagine that you are in the kitchen of your favourite cafe.\n\nYou have a pitcher of your favourite smoothie and the blender is whirring.\n\nThe ingredients in this blender are just as fresh as the memories it invokes.\n\nThey are simple, too: a couple of scoops of your favourite fruit, a few cups of water, and a touch of honey.\n\nWith these ingredients, you can make any kind of smoothie that you want want you can even add a spoonful of peanut butter for a protein boost.',
+                style: kTaglineTextStyle.copyWith(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/modules/health_tips/widgets/tips_card.dart
+++ lib/modules/health_tips/widgets/tips_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hack_with_io/app/app.dart';
+import 'package:hack_with_io/modules/modules.dart';
 
 class TipsCard extends StatelessWidget {
   const TipsCard({
@@ -18,41 +19,52 @@ class TipsCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 5.0),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Container(
-          height: 90,
-          decoration: BoxDecoration(
-            color: colour,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return const MyTipsScreen();
+              },
             ),
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    text,
-                    style: kTaglineTextStyle.copyWith(
-                      color: AppColors.kBlackColor,
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
+          );
+        },
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Container(
+            height: 90,
+            decoration: BoxDecoration(
+              color: colour,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      text,
+                      style: kTaglineTextStyle.copyWith(
+                        color: AppColors.kBlackColor,
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  ),
-                  SvgPicture.asset(
-                    imageUrl,
-                    height: 90,
-                    width: 90,
-                  ),
-                ],
+                    SvgPicture.asset(
+                      imageUrl,
+                      height: 90,
+                      width: 90,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
